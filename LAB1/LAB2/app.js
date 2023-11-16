c5b672c94ac45f5ae1d46491d9ff913196604908
Tammarat123 <650112418013@bru.ac.th>
2023-11-16 15:52:06 +0700

    LAB2

---
+++ LAB1/LAB2/app.js
@@ -0,0 +1,23 @@
+
+var num = 10;
+  let name = "win"
+  age = 20;
+
+  fruit = ["apple","mango","tangmo"];
+  obj ={name:"Tammarat", age:19, tel:"123-456"}
+
+  data = {adress:["11", "jira","Buriram",31000]. name: "John" };
+  console.log(fruit[1]);
+  console.log(obj.tel);
+  console.log(data.adress[2]);
+
+  document.getElementById("msg"),innerHTML = data.adress[2];
+
+  let longTxt = data.name + " . " + fruit[0];
+
+  longTxt = `${data.name} :
+             ${fruit[1]}`;
+  $(Function(){
+    $("#msg").html(longTxt);
+  }); //jquery ready
+
