b0eb7a3e90e282daf6f3962829610e01ef2b3043
Tammarat123 <650112418013@whatever.com>
2024-01-23 15:46:58 +0700

    LAB8

---
+++ LAB8/local.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+http
+  .createServer(function(req, res){
+    res.writeHead(200, {"content Type": "text/plain"});
+    res.write("Hello World");
+    res.end();
+  })
+  .listen(3000)
